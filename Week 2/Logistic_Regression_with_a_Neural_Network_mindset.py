61281ca1efb251530cfe59331771cd9a55fa4860
satiriorn <satiriorn@gmail.com>
2020-05-18 17:09:56 +0300

    week 2 task 2

--- Week 2/Logistic_Regression_with_a_Neural_Network_mindset.py
+++ Week 2/Logistic_Regression_with_a_Neural_Network_mindset.py
@@ -5,7 +5,7 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
-#%matplotlib inline
+
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 # Example of a picture
 index = 25
@@ -25,3 +25,133 @@ print ("train_set_x shape: " + str(train_set_x_orig.shape))
 print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
+# Reshape the training and test examples
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+print("======================== 4 - Building the parts of our algorithm ==========")
+print("======================= 4.1 - Helper functions ===========================")
+print("sigmoid([0, 2]) = " + str(sigmoid(np.array([0, 2]))))
+
+"""the end of the first part"""
+
+
+def initialize_with_zeros(dim):
+    w = np.zeros(shape=(dim, 1))
+    b = 0
+    assert (w.shape == (dim, 1))
+    assert (isinstance(b, float) or isinstance(b, int))
+    return w, b
+
+"""the end of the second part"""
+
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T, X) + b)  # compute activation
+    cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)))  # compute cost
+    dw = (1 / m) * np.dot(X, (A - Y).T)
+    db = (1 / m) * np.sum(A - Y)
+
+    assert (dw.shape == w.shape)
+    assert (db.dtype == float)
+    cost = np.squeeze(cost)
+    assert (cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+#the end of the third part
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    costs = []
+
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w -= learning_rate * dw
+        b -= learning_rate * db
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+    params = {"w": w,
+              "b": b}
+    grads = {"dw": dw,
+             "db": db}
+    return params, grads, costs
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+
+    assert (Y_prediction.shape == (1, m))
+    return Y_prediction
+
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+print ("predictions = " + str(predict(w, b, X)))
+#the end of the fourth part
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    # Predict test/train set examples (≈ 2 lines of code)
+        Y_prediction_test = predict(w, b, X_test)
+        Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+
