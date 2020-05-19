edf67f866209d1a553808fa38c12fb41203f7f41
satiriorn <satiriorn@gmail.com>
2020-05-19 20:43:10 +0300

    week 3 task 1

---
+++ Week 3/Planar_data_classification_with_onehidden_layer.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+
+np.random.seed(1) #
